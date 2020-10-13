7c4850507fdf19506bcdc22ab5f766cf61860617
Hayden <haydenwill95@gmail.com>
2020-10-14 10:20:13 +1300

    YUP

--- use-context/src/App.js
+++ use-context/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { useContext } from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 
+const NumberContext = React.createContext();
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NumberContext.Provider value={45}>
+      <div>
+        <Display />
+      </div>
+    </NumberContext.Provider>
   );
 }
+
+function Display() {
+  const value = useContext(NumberContext);
+  return <div>The answer is {value}.</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
 
 export default App;
